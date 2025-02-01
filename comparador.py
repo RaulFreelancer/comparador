4c2926ae8c864e7486d6a6f931cadf5bb66df6ac
RaulFreelancer <raul.cubo.999@gmail.com>
2025-02-01 17:31:44 -0300

    Add files via upload

---
+++ comparador.py
@@ -0,0 +1,8 @@
+x = input("numero 1: ")
+y = input("numero 2: ")
+if x == y:
+    print("num1 é igual a num2")
+elif x > y:
+    print("num1 é maior que num2")
+elif x < y:
+    print("num1 é menor que num2")
